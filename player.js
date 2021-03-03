28d81d9fecd4419698647195012c37488f56296c
yashvir24 <76519857+yashvir24@users.noreply.github.com>
2021-03-03 17:17:50 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,16 @@
+class Player{
+    constructor(){
+
+    }
+    getCount(){
+       var playerCountRef = database.ref('playerCount');
+       playerCountRef.on("value",(data)=>{
+           playerCount = data.val();
+       })
+    }
+    updateCount(count){
+      database.ref('/').update({
+          playerCount:count
+      })
+    }
+}
